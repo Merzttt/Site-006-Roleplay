596c966e16a6cf7c6a2be2ed3d2cad46777596df
Merzt <120368875+Merzttt@users.noreply.github.com>
2023-01-04 14:50:28 +1000

    New features
    Antimod method 1 and 2 |  Report spam (Cover up traces) | Fake exploiter (frames someone else for exploiting) | Kill All + Safe Kill All + Kill All Test Subjects

--- Site-006-Roleplay.lua
+++ Site-006-Roleplay.lua
@@ -1,5 +1,3 @@
--- https://github.com/shlexware/Rayfield/blob/main/Documentation.md
-
 local Rayfield = loadstring(game:HttpGet("https://raw.githubusercontent.com/shlexware/Rayfield/main/source"))()
 
 local Window =
@@ -56,12 +54,12 @@ local Toggle =
             _G.ChatDetector = Value
 
             while _G.ChatDetector do
-                print("Chat Detecting...")
+                print("User Detecting...")
 
                 for i, v in pairs(game.Players:GetPlayers()) do
                     if
                         string.match(v.UserId, "38152945") or string.match(v.UserId, "938606828") or
-                            string.match(v.UserId, "1666262200")
+                            string.match(v.UserId, "1666262200") or string.match(v.UserId, "1401904438")
                      then
                         game:GetService("StarterGui"):SetCore(
                             "SendNotification",
@@ -101,6 +99,37 @@ local Toggle =
                         )
                     end
                 end
+                wait(1)
+            end
+        end
+    }
+)
+
+local Toggle =
+    Tab:CreateToggle(
+    {
+        Name = "Report all (Cover up traces)",
+        CurrentValue = false,
+        Flag = "Toggle1", -- A flag is the identifier for the configuration file, make sure every element has a different flag if you're using configuration saving to ensure no overlaps
+        Callback = function(Value)
+            _G.Report = Value
+
+            while _G.Report do
+                print("Reporting all...")
+
+                for i,v in pairs(game.Players:GetPlayers()) do
+					if v and v.Character and v.Character:FindFirstChild("Head") and v.Character.Name ~= game.Players.LocalPlayer.Name then
+
+						local args = {
+							[1] = v.UserId,
+							[2] = v.Name,
+							[3] = "Exploiting"
+						}
+
+						game:GetService("ReplicatedStorage").Events.Remotes.NewReport:FireServer(unpack(args))
+
+					end
+				end
                 wait(1)
             end
         end
@@ -198,6 +227,75 @@ local Input =
         Callback = function(Text)
             for i, v in pairs(game.Players:GetPlayers()) do
                 if v.Name == Text or v.DisplayName == Text and v.Name ~= game.Players.LocalPlayer.Name then
+                    local args = {
+                        [1] = Vector3.new(14.122259140014648, -55.53848648071289, -15.409262657165527),
+                        [2] = v.Character.Head,
+                        [3] = CFrame.new(
+                            Vector3.new(13.51423454284668, -55.00370788574219, -15.545818328857422),
+                            Vector3.new(0.3440513610839844, -0.0569915771484375, -0.9372196197509766)
+                        )
+                    }
+
+                    game:GetService("ReplicatedStorage").Events.Remotes.Guns.CreateInstance:FireServer(unpack(args))
+
+                    local args = {
+                        [1] = Vector3.new(14.122259140014648, -55.53848648071289, -15.409262657165527),
+                        [2] = v.Character.Head,
+                        [3] = CFrame.new(
+                            Vector3.new(13.51423454284668, -55.00370788574219, -15.545818328857422),
+                            Vector3.new(0.3440513610839844, -0.0569915771484375, -0.9372196197509766)
+                        )
+                    }
+
+                    game:GetService("ReplicatedStorage").Events.Remotes.Guns.CreateInstance:FireServer(unpack(args))
+
+                    local args = {
+                        [1] = Vector3.new(14.122259140014648, -55.53848648071289, -15.409262657165527),
+                        [2] = v.Character.Head,
+                        [3] = CFrame.new(
+                            Vector3.new(13.51423454284668, -55.00370788574219, -15.545818328857422),
+                            Vector3.new(0.3440513610839844, -0.0569915771484375, -0.9372196197509766)
+                        )
+                    }
+
+                    game:GetService("ReplicatedStorage").Events.Remotes.Guns.CreateInstance:FireServer(unpack(args))
+
+                    local args = {
+                        [1] = Vector3.new(14.122259140014648, -55.53848648071289, -15.409262657165527),
+                        [2] = v.Character.Head,
+                        [3] = CFrame.new(
+                            Vector3.new(13.51423454284668, -55.00370788574219, -15.545818328857422),
+                            Vector3.new(0.3440513610839844, -0.0569915771484375, -0.9372196197509766)
+                        )
+                    }
+
+                    game:GetService("ReplicatedStorage").Events.Remotes.Guns.CreateInstance:FireServer(unpack(args))
+
+                    local args = {
+                        [1] = Vector3.new(14.122259140014648, -55.53848648071289, -15.409262657165527),
+                        [2] = v.Character.Head,
+                        [3] = CFrame.new(
+                            Vector3.new(13.51423454284668, -55.00370788574219, -15.545818328857422),
+                            Vector3.new(0.3440513610839844, -0.0569915771484375, -0.9372196197509766)
+                        )
+                    }
+
+                    game:GetService("ReplicatedStorage").Events.Remotes.Guns.CreateInstance:FireServer(unpack(args))
+                end
+            end
+        end
+    }
+)
+
+local Input =
+    Tab:CreateInput(
+    {
+        Name = "Fake Exploiter:",
+        PlaceholderText = "Username",
+        RemoveTextAfterFocusLost = false,
+        Callback = function(Text)
+            for i, v in pairs(game.Players:GetPlayers()) do
+                if v and v.Character and v.Character:FindFirstChild("Head") and v.Name ~= Text then
                     local args = {
                         [1] = Vector3.new(14.122259140014648, -55.53848648071289, -15.409262657165527),
                         [2] = v.Character.Head,
@@ -461,6 +559,109 @@ local Button =
     }
 )
 
+local Button =
+    Tab:CreateButton(
+    {
+        Name = "Kill Test Subjects (MUST HOLD A GUN)",
+        Callback = function()
+            for i, v in pairs(game.Players:GetPlayers()) do
+                if
+                    v and v.Character and v.Character:FindFirstChild("Head") and
+                        v.Character.Name ~= game.Players.LocalPlayer.Name and v.Team.Name == "Test Subject"
+                 then
+                    local args = {
+                        [1] = Vector3.new(14.122259140014648, -55.53848648071289, -15.409262657165527),
+                        [2] = v.Character.Head,
+                        [3] = CFrame.new(
+                            Vector3.new(13.51423454284668, -55.00370788574219, -15.545818328857422),
+                            Vector3.new(0.3440513610839844, -0.0569915771484375, -0.9372196197509766)
+                        )
+                    }
+
+                    game:GetService("ReplicatedStorage").Events.Remotes.Guns.CreateInstance:FireServer(unpack(args))
+
+                    local args = {
+                        [1] = Vector3.new(14.122259140014648, -55.53848648071289, -15.409262657165527),
+                        [2] = v.Character.Head,
+                        [3] = CFrame.new(
+                            Vector3.new(13.51423454284668, -55.00370788574219, -15.545818328857422),
+                            Vector3.new(0.3440513610839844, -0.0569915771484375, -0.9372196197509766)
+                        )
+                    }
+
+                    game:GetService("ReplicatedStorage").Events.Remotes.Guns.CreateInstance:FireServer(unpack(args))
+
+                    local args = {
+                        [1] = Vector3.new(14.122259140014648, -55.53848648071289, -15.409262657165527),
+                        [2] = v.Character.Head,
+                        [3] = CFrame.new(
+                            Vector3.new(13.51423454284668, -55.00370788574219, -15.545818328857422),
+                            Vector3.new(0.3440513610839844, -0.0569915771484375, -0.9372196197509766)
+                        )
+                    }
+
+                    game:GetService("ReplicatedStorage").Events.Remotes.Guns.CreateInstance:FireServer(unpack(args))
+
+                    local args = {
+                        [1] = Vector3.new(14.122259140014648, -55.53848648071289, -15.409262657165527),
+                        [2] = v.Character.Head,
+                        [3] = CFrame.new(
+                            Vector3.new(13.51423454284668, -55.00370788574219, -15.545818328857422),
+                            Vector3.new(0.3440513610839844, -0.0569915771484375, -0.9372196197509766)
+                        )
+                    }
+
+                    game:GetService("ReplicatedStorage").Events.Remotes.Guns.CreateInstance:FireServer(unpack(args))
+
+                    local args = {
+                        [1] = Vector3.new(14.122259140014648, -55.53848648071289, -15.409262657165527),
+                        [2] = v.Character.Head,
+                        [3] = CFrame.new(
+                            Vector3.new(13.51423454284668, -55.00370788574219, -15.545818328857422),
+                            Vector3.new(0.3440513610839844, -0.0569915771484375, -0.9372196197509766)
+                        )
+                    }
+
+                    game:GetService("ReplicatedStorage").Events.Remotes.Guns.CreateInstance:FireServer(unpack(args))
+
+                    local args = {
+                        [1] = Vector3.new(14.122259140014648, -55.53848648071289, -15.409262657165527),
+                        [2] = v.Character.Head,
+                        [3] = CFrame.new(
+                            Vector3.new(13.51423454284668, -55.00370788574219, -15.545818328857422),
+                            Vector3.new(0.3440513610839844, -0.0569915771484375, -0.9372196197509766)
+                        )
+                    }
+
+                    game:GetService("ReplicatedStorage").Events.Remotes.Guns.CreateInstance:FireServer(unpack(args))
+
+                    local args = {
+                        [1] = Vector3.new(14.122259140014648, -55.53848648071289, -15.409262657165527),
+                        [2] = v.Character.Head,
+                        [3] = CFrame.new(
+                            Vector3.new(13.51423454284668, -55.00370788574219, -15.545818328857422),
+                            Vector3.new(0.3440513610839844, -0.0569915771484375, -0.9372196197509766)
+                        )
+                    }
+
+                    game:GetService("ReplicatedStorage").Events.Remotes.Guns.CreateInstance:FireServer(unpack(args))
+
+                    local args = {
+                        [1] = Vector3.new(14.122259140014648, -55.53848648071289, -15.409262657165527),
+                        [2] = v.Character.Head,
+                        [3] = CFrame.new(
+                            Vector3.new(13.51423454284668, -55.00370788574219, -15.545818328857422),
+                            Vector3.new(0.3440513610839844, -0.0569915771484375, -0.9372196197509766)
+                        )
+                    }
+
+                    game:GetService("ReplicatedStorage").Events.Remotes.Guns.CreateInstance:FireServer(unpack(args))
+                end
+            end
+        end
+    }
+)
+
 local Tab = Window:CreateTab("Teams", 4483362458)
 local Section = Tab:CreateSection("Main")
 
@@ -545,21 +746,15 @@ local Button =
     {
         Name = "Create Safe Zone",
         Callback = function()
-            game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(515, -52, 465)
-            game.Players.LocalPlayer.Character.UpperTorso.Anchored = true
-            wait(1)
-            game.Players.LocalPlayer.Character.UpperTorso.Anchored = false
-
-            wait(2)
-
-            game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(10, -65, -19)
-            game.Players.LocalPlayer.Character.UpperTorso.Anchored = true
-
-            local g = Instance.new("Part")
+        	
+        	local g = Instance.new("Part")
             g.Parent = workspace
             g.Anchored = true
             g.Position = Vector3.new(-67, -69, -16)
             g.Size = Vector3.new(289, 0, 167)
+        	
+			game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(10, -65, -19)
+            game.Players.LocalPlayer.Character.UpperTorso.Anchored = true
 
             wait(1)
             game.Players.LocalPlayer.Character.UpperTorso.Anchored = false
@@ -576,4 +771,3 @@ local Button =
 
 local Tab = Window:CreateTab("Info", 4483362458)
 local Section = Tab:CreateSection("Main")
-
